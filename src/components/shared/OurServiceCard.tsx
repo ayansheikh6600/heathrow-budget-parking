442a057408a4ba63a68dd407dd8ddcde4d4ba93a
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-07-14 05:59:18 +0500

    "Done"

--- src/components/shared/OurServiceCard.tsx
+++ src/components/shared/OurServiceCard.tsx
@@ -2,7 +2,10 @@
 import Image from 'next/image';
 import meetingImage from '../public/meeting.png'; // apni image ko yahan import karen
 
-const OurServiceCard = () => {
+const OurServiceCard = ({...props}) => {
+
+    const {data} = props
+
     return (
         <div
 
@@ -11,14 +14,14 @@ const OurServiceCard = () => {
         >
             <div className={`absolute bottom-0 flex flex-col justify-end text-white left-0 right-0 w-full object-cover h-full bg-black bg-opacity-30 rounded-lg`}>
                 <div className="relative p-4 z-10">
-                    <h2 className="text-lg font-bold mb-2 sm:text-xl">Meet And Greet</h2>
+                    <h2 className="text-lg font-bold mb-2 sm:text-xl">{data?.title || "Meet And Greet"}</h2>
                     <p className="text-xs sm:text-sm">
-                        Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s.
+                        {data?.desc || "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}
                     </p>
                 </div>
             </div>
             <Image
-                src={"/service2.png"}
+                src={data?.image || "/service2.png"}
                 alt={""}
                 className="w-full h-full  rounded-xl"
                 width={520}
--- src/components/widgets/OurServiceSection.tsx
+++ src/components/widgets/OurServiceSection.tsx
@@ -2,6 +2,42 @@ import React from 'react'
 import OurServiceCard from '../shared/OurServiceCard'
 
 const OurServiceSection = () => {
+
+
+    const data = [
+        {
+            title :"Meet and Greet",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service5.png"
+        },
+        {
+            title :"PARK AND RIDE",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service2.png"
+        },
+        {
+            title :"ONSITE PARKING",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service3.png"
+        },
+        {
+            title :"Airport Transfer",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service4.png"
+        },
+        {
+            title :"Long & Short Stay",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service5.png"
+        },
+        {
+            title :"Premium & Valet Parking",
+            desc : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+            image:"/service6.png"
+        },
+    ]
+
+
   return (
     <div className='sm:container w-full p-2 flex flex-col gap-4'>
         
@@ -11,12 +47,9 @@ const OurServiceSection = () => {
             </h2>
         </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-            <OurServiceCard/>
-            <OurServiceCard/>
-            <OurServiceCard/>
-            <OurServiceCard/>
-            <OurServiceCard/>
-            <OurServiceCard/>
+            {data.map((item, index)=>{
+                return <OurServiceCard key={index} data={item}/>
+            })}
         </div>
     </div>
   )
